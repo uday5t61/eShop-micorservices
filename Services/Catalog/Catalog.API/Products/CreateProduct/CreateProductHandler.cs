fd56540f92eb5af4b4d42ef2e3c6d368a04693d0
uday5t61 <uday5t61@gmail.com>
2025-05-03 09:40:10 +0530

    Add project files.

---
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿
+
+using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name,List<string> Category,string Description,string ImageFile,decimal price)
+    : ICommand<CreateProductResponse>;
+public record CreateProductResponse(Guid Id);
+public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
+{
+    public Task<CreateProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
